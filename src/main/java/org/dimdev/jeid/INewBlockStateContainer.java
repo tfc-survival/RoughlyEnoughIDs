61d98c2b568433cc9fc51f63979a1659391953f5
Timo Ley <timo.ley@tilera.dev>
2023-04-04 13:47:33 +0200

    feat: convert chunks from NEID format

--- src/main/java/org/dimdev/jeid/INewBlockStateContainer.java
+++ src/main/java/org/dimdev/jeid/INewBlockStateContainer.java
@@ -1,6 +1,9 @@
 package org.dimdev.jeid;
 
+import net.minecraft.world.chunk.NibbleArray;
+
 public interface INewBlockStateContainer {
     void setTemporaryPalette(int[] temporaryPalette);
     int[] getTemporaryPalette();
+    void setLegacyAdd2(NibbleArray add2);
 }
