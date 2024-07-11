5fef51521d96f3d4cf89a0a7c97c1e8d62a798c9
hohserg1 <hohserg1@yandex.ru>
2024-07-11 15:25:46 +0300

    review fixes

--- src/main/java/org/dimdev/jeid/mixin/core/world/MixinStateImplementation.java
+++ src/main/java/org/dimdev/jeid/mixin/core/world/MixinStateImplementation.java
@@ -1,19 +1,31 @@
 package org.dimdev.jeid.mixin.core.world;
 
-import com.google.common.collect.*;
-import java.util.*;
-import net.minecraft.block.properties.*;
-import net.minecraft.block.state.BlockStateContainer.*;
-import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.*;
+import com.google.common.collect.ImmutableMap;
+import java.util.Objects;
+import net.minecraft.block.Block;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockStateContainer.StateImplementation;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(StateImplementation.class)
 public class MixinStateImplementation {
 
+    @Unique
     private int hashCodeValue;
 
     @Shadow
+    @Final
+    private Block block;
+
+    @Shadow
+    @Final
     private ImmutableMap<IProperty<?>, Comparable<?>> properties;
 
     @Inject(
@@ -21,7 +33,7 @@ public class MixinStateImplementation {
         at = @At("RETURN")
     )
     private void initHashCode(CallbackInfo ci) {
-        hashCodeValue = Objects.hash(this, properties);
+        hashCodeValue = Objects.hash(block, properties);
     }
 
     /**
